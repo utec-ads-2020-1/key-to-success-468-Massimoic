c4530e0a5e48cfaeb1635ea25e0d0225a007c792
Massimo Imparato <33756597+Massimoic@users.noreply.github.com>
2020-04-10 20:14:29 -0500

    Added files via upload

---
+++ cmake-build-debug/CMakeFiles/KeyToSuccess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KeyToSuccess.dir/main.cpp.obj"
+  "KeyToSuccess.exe"
+  "KeyToSuccess.exe.manifest"
+  "KeyToSuccess.pdb"
+  "libKeyToSuccess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KeyToSuccess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
