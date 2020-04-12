64729f21764560abaa5e92df92df4d4a03c4db1e
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-11 22:06:52 -0500

    quite funciones comentario, arregle contador de ocurrencias y agregue funcion para invertir el mapa y ordenar por numero

--- main.cpp
+++ main.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <map>
-#include <iterator>
 using namespace std;
 
-#define SIZE 26
+map<int,char, greater<int> > invert(map<char,int> mapa) {
+    map<int,char, greater<int> > result;
 
-/*bool checkLetter(map<char,int> mapa, char letter) {
-    bool result = false;
-    for (int i = 0 ; i < mapa.size() ; i++) {
-        if( mapa[i] == letter ) {
-            result = true;
-            return result;
-        }
+    map<char,int> :: iterator itr;
+    for( itr = mapa.begin() ; itr != mapa.end() ; itr++) {
+        result.insert(pair<int,char> (itr->second,itr->first));
     }
     return result;
-}*/
+}
 
-/*vector<char> vec (string text) {
-    int size = sizeof(text) / text[0];
-    vector<char> result[SIZE];
-    for(int i = 0 ; i < size ; i++) {
-        (checkLetter(result, text[i])) ? :
-    }
-    return result;
-
-}*/
-
-map<char,int> countFrequency(string text) {
+map<char,int> countOccurrences(string text) {
     map<char,int> letters;
+    letters.insert(pair<char,int> (text[0],1));
+
     for (char const &c : text) {
         if(letters.find(c) == letters.end())
             letters.insert(pair<char,int> (c, 1));
         else
             letters.find(c)->second++;
     }
+
     return letters;
+}
+
+void printInvertedMap(map<int,char, greater<int>> mapa ) {
+    map<int,char, greater<int>>::iterator itr;
+    for(itr = mapa.begin() ; itr != mapa.end(); itr++) {
+        cout << itr->first << " : " << itr->second << endl;
+    }
 }
 
 void printMap(map<char,int> mapa) {
@@ -49,13 +44,13 @@ int main() {
     char blank;
     string textCoded, textUncoded;
     int n ;
-    //cin >> n >> blank;
 
+    cin >> n >> blank;
     cin >> textUncoded >> textCoded;
 
+    printMap(countOccurrences(textUncoded));
+    printInvertedMap(invert(countOccurrences(textUncoded)));
 
-    printMap(countFrequency(textUncoded));
-    printMap(countFrequency(textCoded));
 
     /*for ( int i = 0 ; i < n ; i++) {
         cin >> textUncoded >> textCoded;
